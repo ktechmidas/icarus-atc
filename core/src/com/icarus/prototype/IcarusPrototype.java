44b657966d7a9e8c0e6c8fa8596ad578857045c6
Benjamin Lee <benjamin.fik.lee@gmail.com>
2016-11-07 13:21:04 -0800

    Addede labels to waypoints

--- core/src/com/icarus/prototype/IcarusPrototype.java
+++ core/src/com/icarus/prototype/IcarusPrototype.java
@@ -4,43 +4,68 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.FileHandleResolver;
+import com.badlogic.gdx.graphics.g2d.freetype.FreetypeFontLoader.FreeTypeFontLoaderParameter;
+import com.badlogic.gdx.graphics.g2d.freetype.FreetypeFontLoader;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGeneratorLoader;
 import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.assets.loaders.SynchronousAssetLoader;
 import com.icarus.prototype.Airport;
 import com.icarus.prototype.AirportLoader;
 import com.icarus.prototype.Waypoint;
+import com.icarus.prototype.Colors;
 
 public class IcarusPrototype extends ApplicationAdapter {
 	private ShapeRenderer shapes;
 	private Airport airport;
+	private BitmapFont labelFont;
+	private SpriteBatch batch;
 	
 	@Override
 	public void create () {
 		AssetManager manager = new AssetManager();
 		FileHandleResolver resolver = new InternalFileHandleResolver();
 		manager.setLoader(Airport.class, new AirportLoader(resolver));
+		manager.setLoader(FreeTypeFontGenerator.class, new FreeTypeFontGeneratorLoader(resolver));
+		manager.setLoader(BitmapFont.class, ".ttf", new FreetypeFontLoader(resolver));
+
 		manager.load("airports/test.json", Airport.class);
+		FreeTypeFontLoaderParameter labelFontParams = new FreeTypeFontLoaderParameter();
+		labelFontParams.fontFileName = "ShareTechMono-Regular.ttf";
+		labelFontParams.fontParameters.size = 40;
+		manager.load("ShareTechMono-Regular.ttf", BitmapFont.class, labelFontParams);
+
 		manager.finishLoading();
 		airport = manager.get("airports/test.json");
+		labelFont = manager.get("ShareTechMono-Regular.ttf");
 		shapes = new ShapeRenderer();
+		batch = new SpriteBatch();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(Colors.colors[0].r, Colors.colors[0].g, Colors.colors[2].b, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		shapes.begin(ShapeRenderer.ShapeType.Filled);
-		shapes.setColor(1, 1, 1, 1);
 		for(Waypoint waypoint: airport.waypoints) {
 			waypoint.draw(shapes);
 		}
 		shapes.end();
+		batch.begin();
+		for(Waypoint waypoint: airport.waypoints) {
+			waypoint.drawLabel(labelFont, batch);
+		}
+			batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		shapes.dispose();
+		batch.dispose();
+		labelFont.dispose();
 	}
-}
+}	
--- core/src/com/icarus/prototype/Waypoint.java
+++ core/src/com/icarus/prototype/Waypoint.java
@@ -1,12 +1,16 @@
 package com.icarus.prototype;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.Align;
 import com.google.gson.JsonObject;
+import com.icarus.prototype.Colors;
 
 class Waypoint {
     public String name;
     public Vector2 position;
-    private float size = 10.0f;
+    private float size = 20.0f;
 
     public Waypoint(String name, Vector2 position) {
         this.name = name;
@@ -19,6 +23,7 @@ class Waypoint {
     }
 
     public void draw(ShapeRenderer shapes) {
+       shapes.setColor(Colors.colors[3]);
        shapes.triangle(
           -0.7f * size + position.x,
           -0.4041f * size + position.y,
@@ -26,5 +31,10 @@ class Waypoint {
           -0.4041f * size + position.y,
           0.0f * size + position.x,
           0.8083f * size + position.y);
+    }
+
+    public void drawLabel(BitmapFont font, SpriteBatch batch) {
+       font.setColor(Colors.colors[3]);
+       font.draw(batch, name, position.x - 100, position.y - 15, 200, Align.center, false);
     }
 }
