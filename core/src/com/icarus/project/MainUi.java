e545710ed34ad9caffb38106c1b4c74e95d15903
lod2000 <lod.wolflovers@gmail.com>
2017-01-03 13:25:26 -0800

    added button drawable

--- core/src/com/icarus/project/MainUi.java
+++ core/src/com/icarus/project/MainUi.java
@@ -1,7 +1,17 @@
 package com.icarus.project;
 
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
 public class MainUi {
     public MainUi(AssetManager assets) {
 
+        Drawable headingDrawable = new TextureRegionDrawable(
+                new TextureRegion((Texture) assets.get("buttons/heading_button.png")));
+        ImageButton headingButton = new ImageButton(headingDrawable);
     }
 }
