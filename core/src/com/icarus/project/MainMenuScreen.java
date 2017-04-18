a855688c940b90fc43e0f193b4448bad55044362
gmcfeeters3345 <guy.mckrone@gmail.com>
2017-04-18 13:33:33 -0700

    Cleaned up main menu

--- core/src/com/icarus/project/MainMenuScreen.java
+++ core/src/com/icarus/project/MainMenuScreen.java
@@ -3,14 +3,11 @@ package com.icarus.project;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.assets.loaders.FileHandleResolver;
-import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -18,47 +15,27 @@ import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.Input;
-import com.sun.glass.ui.EventLoop;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 
-import java.util.Random;
 /**
  * Created by gmcfeeters3345 on 2/14/2017.
  */
 
-public class MainMenuScreen implements Screen, GestureDetector.GestureListener{
+public class MainMenuScreen implements Screen, GestureDetector.GestureListener {
     final ProjectIcarus game;
     public Stage stage;
     AssetManager menuManager = new AssetManager();
-    FileHandleResolver menuResolver = new InternalFileHandleResolver();
-
     OrthographicCamera camera;
     private BitmapFont font = new BitmapFont();
     private SpriteBatch batch;
-    private ImageButton menuButton;
-    private int buttonSize = (int) (100 * Gdx.graphics.getDensity());
-    private int buttonGap = (int) (5 * Gdx.graphics.getDensity());
-    public int statusBarHeight = (int) (25 * Gdx.graphics.getDensity());
-    private Texture playerPortrait1;
-
     private ImageButton playerBtn1;
     private Skin playerBtnSkin1;
     private ImageButton.ImageButtonStyle playerBtnStyle1;
-
-    private ImageButton.ImageButtonStyle backBtnStyle;
     int[] positions = {1240, 150, 235, 500, 600, 500, 975, 500, 440, 200, 785, 200, 1280, 485};
-    int drawCounter = 0;
-    private Texture playerSelected;
-    int selected = -1;
-    public MainMenuScreen(final ProjectIcarus game) {
 
+    public MainMenuScreen(final ProjectIcarus game) {
         this.game = game;
         stage = new Stage();
-        BitmapFont font = new BitmapFont();
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
         batch = new SpriteBatch();
@@ -69,67 +46,35 @@ public class MainMenuScreen implements Screen, GestureDetector.GestureListener{
         playerBtnStyle1.imageDown = playerBtnSkin1.getDrawable("playerBtn1");    //sets the button appearance when it is pressed
         playerBtn1 = new ImageButton(playerBtnStyle1);    //initializes the ImageButton with the created style as a parameter
         playerBtn1.setBounds(positions[2], positions[3], playerBtn1.getWidth(), playerBtn1.getHeight());  //tells the button where to go
-        Gdx.app.log("MainMenuScreen", "Main menu button");
-        playerBtn1.addListener(new InputListener() {
+        playerBtn1.addListener(new InputListener() {//adds listener to check for touch
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
                 return true;
             }
-
             @Override
             public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                game.setScreen(new PIScreen(game));
+                game.setScreen(new PIScreen(game));//switch screen to game state
                 playerBtn1.setDisabled(true);
             }
         });
-        stage.addActor(playerBtn1);
-        //While loop is causeing black screen and am only chekcing once without it
-        /*menuManager.load("buttons/landing_button.png", Texture.class);
-        menuManager.finishLoading();
-        Drawable landingDrawable = new TextureRegionDrawable(
-                new TextureRegion((Texture) menuManager.get("buttons/landing_button.png"))
-        );
-        menuButton = new ImageButton(landingDrawable);
-        menuButton.setSize(buttonSize, buttonSize);
-        menuButton.setPosition(4 * buttonGap + 3 * buttonSize, statusBarHeight + buttonGap);
-        menuButton.addListener(new InputListener(){
-            @Override
-            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
-                Gdx.app.log("MainMenuScreen","Main menu button");
-                return true;
-            }
-            @Override
-            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
-               *//*game.setScreen(new PIScreen(game));
-                dispose();*//*
-
-            }
-        });
-        stage.addActor(menuButton);
-        menuButton.setSize(buttonSize, buttonSize);*/
-
+        stage.addActor(playerBtn1);//adds the button to the stage
         Gdx.input.setInputProcessor(new InputMultiplexer(stage, new GestureDetector(this)));
     }
 
-    public void button() {
-
-        }
-
     @Override
     public void show() {
-
     }
 
     @Override
     public void render(float delta) {
-            Gdx.gl.glClearColor(1, 1, 1, 1);
-            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-            batch.begin();
-            playerBtn1.draw(batch, 1);
-            batch.end();
-            stage.act();
-            stage.draw();
-}
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        playerBtn1.draw(batch, 1);
+        batch.end();
+        stage.act();
+        stage.draw();
+    }
 
 
     @Override
@@ -158,7 +103,8 @@ public class MainMenuScreen implements Screen, GestureDetector.GestureListener{
         batch.dispose();
         font.dispose();
         menuManager.dispose();
-
+        stage.dispose();
+        game.dispose();
     }
 
     @Override
